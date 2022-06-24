55a9f7bb8ac6612fa5df57cfeab202eb05337424
DorkYBru <76796721+DorkYBru@users.noreply.github.com>
2022-06-24 14:18:46 +0000

    nacho spigot not working

--- install.sh
+++ install.sh
@@ -6,22 +6,22 @@ java -version
 
 echo "Minecraft Server Termux installer"
 echo "Select minecraft server version (only 1.8)"
-echo "1.NachoSpigot"
+echo "1.WindSpigot"
 echo "2.Spigot"
 echo "3.PaperMC"
 
 read version
 
 paperlink="https://papermc.io/api/v2/projects/paper/versions/1.8.8/builds/445/downloads/paper-1.8.8-445.jar"
-nacholink="https://transfer.sh/sTU9t8/NachoSpigot.jar"
+nacholink="https://github.com/Wind-Development/WindSpigot/releases/download/v2.1.2-hotfix/WindSpigot-2.1.2.jar"
 spigotlink="https://cdn.getbukkit.org/spigot/spigot-1.8.8-R0.1-SNAPSHOT-latest.jar"
 
 
  if [ $version = 1 ];
     then
-    mkdir NachoServer
+    mkdir WindServer
     cd NachoServer
-    curl $nacholink -o NachoSpigot.jar
+    curl $nacholink
 fi
 
  if [ $version = 2 ];
